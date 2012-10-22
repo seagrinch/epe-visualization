39d74002552c713bce43a91b85737071f7599f05
Sage Lichtenwalner <sage@marine.rutgers.edu>
2012-10-22 15:02:39 -0400

    Upgrade to Glider Web Service to support new db schema

--- app/webroot/proxy_glider.php
+++ app/webroot/proxy_glider.php
@@ -1,21 +1,22 @@
 <?php
 // Provisional Glider Web Service
 //
-// Written by Sage Lichtenwalner, Rutgers University 6/28/12
+// Written by Sage Lichtenwalner, Rutgers University
 // Ocean Observatories Initiative 
 // Education & Public Engagement Implementing Organization
 //
-// Revised 7/6/12
+// Revised 10/22/12
+// Version 0.2
 //
 // This script was adapted (slightly) from the IOOS Sensor Observation Service software
 //   http://sdf.ndbc.noaa.gov/sos/software/
 // However, this service does (yet) not follow SOS conventions, as it currently favors 
-//   glider cast-based querying, rather than time or location-based queries.
+//   cast-based querying for glider data, rather than time or location-based queries.
 //
 // This service provides the following capabilities which can be accessed as follows
-//  proxy_glider.php?request=getdeployments
-//  proxy_glider.php?request=gettrack&deploymentid=246
-//  proxy_glider.php?request=getcast&deploymentid=246&castid=3
+//  proxy_glider.php?request=getDeployments
+//  proxy_glider.php?request=getTrack&deploymentid=246
+//  proxy_glider.php?request=getCast&castid=3
 
 /**
  * Exception Codes 
@@ -41,7 +42,7 @@ function getParams() {
 		} else {
 			$exceptions[] = array(EXCEPT_MISSING_PARAMETER_VALUE,'request',null);
 		}
-		if ( ($params['request'] == 'gettrack') || ($params['request'] == 'getcast') ) {
+		if ( ($params['request'] == 'gettrack') ) {
   		if (array_key_exists('deploymentid',$_GET)) {
     		if (is_numeric($_GET['deploymentid'])) {
     			$params['deploymentid'] = $_GET['deploymentid'];
@@ -72,11 +73,13 @@ function getParams() {
  */ 
 function getDeployments(){
   global $mysqli;
-  $data['header'] = array('id','name','start_time','end_time','casts');
-  $sql = "SELECT id,name,date_format(start_time,'%Y-%m-%dT%TZ') as start_time,date_format(end_time,'%Y-%m-%dT%TZ') as end_time,casts FROM deployments ORDER by start_time";
+  $data['header'] = array('id','start_time','end_time','glider_id','name');
+  $sql = "SELECT deployment_id,date_format(start_date,'%Y-%m-%dT%TZ') as start_time,date_format(end_date,'%Y-%m-%dT%TZ') as end_time,glider_id,name 
+    FROM epe_deployments 
+    ORDER by start_time";
   if ($result = $mysqli->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-      $data['obs'][] = array('id'=>$row['id'], 'name'=>$row['name'], 'start_time'=>$row['start_time'], 'end_time'=>$row['end_time'], 'casts'=>$row['casts']);
+      $data['obs'][] = array('id'=>$row['deployment_id'], 'start_time'=>$row['start_time'], 'end_time'=>$row['end_time'], 'glider_id'=>$row['glider_id'], 'name'=>$row['name'] );
     }
     $result->free();
   }
@@ -86,7 +89,7 @@ function getDeployments(){
 	if ($csv) {
 		fputcsv($csv,$data['header']);
 		foreach($data['obs'] as $ob) {
-		  fputcsv($csv,array($ob['id'],$ob['name'],$ob['start_time'],$ob['end_time'],$ob['casts']));
+		  fputcsv($csv,array($ob['id'],$ob['start_time'],$ob['end_time'],$ob['glider_id'],$ob['name']));
 		}
     fclose($csv);
   }
@@ -98,24 +101,33 @@ function getDeployments(){
  */ 
 function getTrack($deployment_id){
   global $mysqli;
-  $data['header'] = array('deployment_id','obsdate','latitude','longitude','profile_id','direction');
-  $sql = "SELECT deployment_id,date_format(observation_time,'%Y-%m-%dT%TZ') as obsdate,latitude,longitude,profile_id,direction 
-    FROM cast_data 
-    WHERE deployment_id='$deployment_id' 
-    GROUP BY profile_id";
+  $data['header'] = array('obsdate','latitude','longitude','profile_id','direction');
+  $sql = "SELECT * 
+    FROM epe_deployments 
+    WHERE deployment_id='$deployment_id'";
+  if ($result = $mysqli->query($sql)) {
+    $deployment = $result->fetch_assoc();
+    $result->free();
+  }
+  $sql = "SELECT profile_id, date_format(profile_time,'%Y-%m-%dT%TZ') as obsdate, profile_lat, profile_lon, profile_direction 
+    FROM epe_profiles 
+    WHERE glider_id = " . $deployment['glider_id'] . " AND profile_time >= '" . $deployment['start_date'] . "' AND profile_time <= '" . $deployment['end_date'] . "'";
   if ($result = $mysqli->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-      $data['obs'][] = array('deployment_id'=>$row['deployment_id'], 'obsdate'=>$row['obsdate'], 'latitude'=>$row['latitude'], 'longitude'=>$row['longitude'], 'profile_id'=>$row['profile_id'], 'direction'=>$row['direction']);
+      $data['obs'][] = array('obsdate'=>$row['obsdate'], 'latitude'=>$row['profile_lat'], 'longitude'=>$row['profile_lon'], 'profile_id'=>$row['profile_id'], 'direction'=>$row['profile_direction']);
     }
     $result->free();
   }
+
 	header('Content-type: text/csv; header=present');
 	header('Content-Disposition: attachment; filename="gliderdata.csv"');
 	$csv = @fopen('php://output','w');
 	if ($csv) {
 		fputcsv($csv,$data['header']);
-		foreach($data['obs'] as $ob) {
-		  fputcsv($csv,array($ob['deployment_id'],$ob['obsdate'],$ob['latitude'],$ob['longitude'],$ob['profile_id'],$ob['direction']));
+		if (isset($data['obs'])>0) {
+		  foreach($data['obs'] as $ob) {
+		    fputcsv($csv,array($ob['obsdate'],$ob['latitude'],$ob['longitude'],$ob['profile_id'],$ob['direction']));
+		  }
 		}
     fclose($csv);
   }
@@ -124,36 +136,37 @@ function getTrack($deployment_id){
 /**
  * getCast
  */ 
-function getCast($deployment_id,$profile_id){
+function getCast($profile_id){
   global $mysqli;
-  $data['header'] = array('deployment_id','obsdate','depth','latitude','longitude',
-    'sea_water_temperature','sea_water_salinity','sea_water_density','sci_bb3slo_b470_scaled',
-    'sci_bb3slo_b532_scaled','sci_bb3slo_b660_scaled','sci_bbfl2s_cdom_scaled','sci_bbfl2s_chlor_scaled','profile_id','direction');
-  $sql = "SELECT deployment_id,date_format(observation_time,'%Y-%m-%dT%TZ') as obsdate,depth,latitude,longitude,
-    sea_water_temperature,sea_water_salinity,sea_water_density,sci_bb3slo_b470_scaled,
-    sci_bb3slo_b532_scaled,sci_bb3slo_b660_scaled,sci_bbfl2s_cdom_scaled,sci_bbfl2s_chlor_scaled,profile_id,direction 
-    FROM cast_data 
-    WHERE deployment_id='$deployment_id' AND profile_id='$profile_id'
-    ORDER BY depth";
+  $data['header'] = array('data_id', 'profile_id', 'obsdate', 'latitude', 'longitude', 'depth', 
+    'tempwat', 'condwat', 'pracsal', 'density', 'optparw', 'flubsct', 'cdomflo', 'chlaflo', 'doconcs');
+    
+  $sql = "SELECT data_id, profile_id, date_format(observation_time,'%Y-%m-%dT%TZ') as obsdate, latitude,longitude, preswat,
+    tempwat, condwat, pracsal, density, optparw, flubsct, cdomflo, chlaflo, doconcs 
+    FROM epe_profile_data 
+    WHERE profile_id='$profile_id'
+    ORDER BY preswat";
+
   if ($result = $mysqli->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-      $data['obs'][] = array('deployment_id'=>$row['deployment_id'], 'obsdate'=>$row['obsdate'], 'depth'=>$row['depth'], 
-        'latitude'=>$row['latitude'], 'longitude'=>$row['longitude'], 
-        'sea_water_temperature'=>$row['sea_water_temperature'], 'sea_water_salinity'=>$row['sea_water_salinity'], 
-        'sea_water_density'=>$row['sea_water_density'], 'sci_bb3slo_b470_scaled'=>$row['sci_bb3slo_b470_scaled'], 
-        'sci_bb3slo_b532_scaled'=>$row['sci_bb3slo_b532_scaled'], 'sci_bb3slo_b660_scaled'=>$row['sci_bb3slo_b660_scaled'], 
-        'sci_bbfl2s_cdom_scaled'=>$row['sci_bbfl2s_cdom_scaled'], 'sci_bbfl2s_chlor_scaled'=>$row['sci_bbfl2s_chlor_scaled'], 
-        'profile_id'=>$row['profile_id'], 'direction'=>$row['direction']);
+      $data['obs'][] = array('data_id'=>$row['data_id'], 'profile_id'=>$row['profile_id'], 'obsdate'=>$row['obsdate'],  
+        'latitude'=>$row['latitude'], 'longitude'=>$row['longitude'], 'preswat'=>$row['preswat'],
+        'tempwat'=>$row['tempwat'], 'condwat'=>$row['condwat'], 'pracsal'=>$row['pracsal'], 'density'=>$row['density'], 
+        'optparw'=>$row['optparw'], 'flubsct'=>$row['flubsct'], 
+        'cdomflo'=>$row['cdomflo'], 'chlaflo'=>$row['chlaflo'], 'doconcs'=>$row['doconcs']);
     }
     $result->free();
   }
+
 	header('Content-type: text/csv; header=present');
 	header('Content-Disposition: attachment; filename="gliderdata.csv"');
 	$csv = @fopen('php://output','w');
 	if ($csv) {
 		fputcsv($csv,$data['header']);
-		foreach($data['obs'] as $ob) {
-		  fputcsv($csv,array($ob['deployment_id'],$ob['obsdate'],$ob['depth'],$ob['latitude'],$ob['longitude'],$ob['sea_water_temperature'],$ob['sea_water_salinity'],$ob['sea_water_density'],$ob['sci_bb3slo_b470_scaled'],$ob['sci_bb3slo_b532_scaled'],$ob['sci_bb3slo_b660_scaled'],$ob['sci_bbfl2s_cdom_scaled'],$ob['sci_bbfl2s_chlor_scaled'],$ob['profile_id'],$ob['direction']));
+		if (isset($data['obs'])>0) {
+  		foreach($data['obs'] as $ob) {
+  		  fputcsv($csv,array($ob['data_id'],$ob['profile_id'],$ob['obsdate'],$ob['latitude'],$ob['longitude'],$ob['preswat'],$ob['tempwat'],$ob['condwat'],$ob['pracsal'],$ob['density'],$ob['optparw'],$ob['flubsct'],$ob['cdomflo'],$ob['chlaflo'],$ob['doconcs']));
+      }
 		}
     fclose($csv);
   }
@@ -205,7 +218,7 @@ if (count($exceptions) == 0) {
   switch($params['request']) {
     case 'getdeployments': $data = getDeployments(); break;
     case 'gettrack': $data = getTrack($params['deploymentid']); break;
-    case 'getcast': $data = getCast($params['deploymentid'],$params['castid']); break;
+    case 'getcast': $data = getCast($params['castid']); break;
     default: $exceptions[] = array(EXCEPT_OPERATION_NOT_SUPPORTED,$params['request'],null); break;
   }
 }
